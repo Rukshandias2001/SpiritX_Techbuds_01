4358d35d066ccf079ab4808daed9d4b9f4ebc96d
Ronella Dias <ronelladias17@gmail.com>
2025-03-08 10:27:04 +0530

    done with the sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
 
 function App() {
- 
-
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
