f50706fcd55ac788f919e98390b59b3fa8626198
Onellaa <onelladias17@gmail.com>
2025-03-08 11:01:53 +0530

    Done with the login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-
+import Login from './pages/Login'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 function App() {
+
  
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   )
 }
